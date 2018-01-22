9ecb797315548957d08a100c1174178801757a3f
miao.qiang <miao.qiang04894@westone.com.cn>
2018-01-22 17:00:36 +0800

    add some pages

--- shunjie/shunjie/settings.py
+++ shunjie/shunjie/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&=h)(@r%=+wm^lyd(%d)y)#lhb3iuzravmelt6@dfpf5#luk*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
 DEBUG = True
-
 ALLOWED_HOSTS = ['10.3.1.41']
 
 
